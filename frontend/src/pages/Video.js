125e86485da77483ffb5f1695b6ec2b8d29caec4
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-26 08:35:38 +0600

    player component added

---
+++ frontend/src/pages/Video.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../components/footer/Footer';
+import Navbar from '../components/navbar/Navbar';
+
+const Video = () => {
+    return (
+        <>
+            <Navbar />
+                
+            <Footer />
+        </>
+    );
+};
+
+export default Video;
