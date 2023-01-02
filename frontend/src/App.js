d78e25fed919400b6b82ec01fa49b99b9bf13b31
sadikhossain69 <sadikhossain433@gmail.com>
2023-01-02 17:00:04 +0600

    react routes setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home';
@@ -8,6 +9,13 @@ function App() {
   return (
     <>
       <Navbar />
+
+      {/* Router setup */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/videos/:videoId' element={<Video />} />
+      </Routes>
+
       <Footer />
     </>
   );
